2680eb558eb7c4677de63a15137e0365e12c16c7
jpd61 <jpd@dewoodyoil.com>
2020-04-19 14:27:30 -0500

    updated single repo and single js

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -8,6 +9,10 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
             displayIssues(data);
+
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
         });
         } else {
             alert("There was a problem with your request!");
@@ -47,6 +52,16 @@ var displayIssues = function(issues) {
         issueEl.appendChild(typeEl);
         issueContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
 };
 
 getRepoIssues("facebook/react");
